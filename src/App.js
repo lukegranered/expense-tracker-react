b7f013ecd91a89fd98d42355ea36b68c213cdc71
Luke G <lukejgranered@gmail.com>
2022-07-11 11:14:51 -0500

    init commit, components added

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import { Header } from './components/Header';
+import { Balance } from './components/Balance';
+import { IncomeExpenses } from './components/IncomeExpenses';
+import { TransactionList } from './components/TransactionList';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <div className='container'>
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
